0d437c8f12a2a47844c8b08f836fb3396c974141
Aditya Sai Srinivas <srinivaa@atcremers55.in.tum.de>
2022-10-11 11:07:26 +0200

    fix : renamed the args

--- core/models/utils_arap/arap_potential.py
+++ core/models/utils_arap/arap_potential.py
@@ -5,12 +5,13 @@
 
 import torch.nn.functional
 from core.models.utils_arap.base_tools import *
+import numpy as np
 #from param import *
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-def arap_exact(vert_diff_t, vert_diff_0, neigh, n_vert):
-    S_neigh = torch.bmm(vert_diff_t.unsqueeze(2), vert_diff_0.unsqueeze(1))
-
+def arap_exact(vert_diff_cdc, vert_diff_query, neigh, n_vert):
+    S_neigh = torch.bmm(vert_diff_cdc.unsqueeze(2),vert_diff_query.unsqueeze(1))
+    
     S = my_zeros([n_vert, 3, 3])
 
     S = torch.index_add(S, 0, neigh[:, 0], S_neigh)
@@ -31,23 +32,26 @@ def arap_exact(vert_diff_t, vert_diff_0, neigh, n_vert):
     return R
 
 
-def arap_energy_exact(vert_t, vert_0, neigh, lambda_reg_len=1e-6):
-    n_vert = vert_t.shape[0]
+def arap_energy_exact(vert_cdc, vert_query, neigh, lambda_reg_len=1e-6):
+    n_vert = vert_cdc.shape[0]
+    
+    vert_diff_cdc = torch.sub(vert_cdc[neigh[:,0]],vert_cdc[neigh[:,1]])
+    vert_diff_query = torch.sub(vert_query[neigh[:,0]],vert_query[neigh[:,1]])
 
-    vert_diff_t = vert_t[neigh[:, 0], :] - vert_t[neigh[:, 1], :]
-    vert_diff_0 = vert_0[neigh[:, 0], :] - vert_0[neigh[:, 1], :]
-
-    R_t = arap_exact(vert_diff_t, vert_diff_0, neigh, n_vert)
-
+    # Beginning of exact minimization scheme
+    # Assuming deformed coordinates (cdc coords) are correct, find value of rotation matrix that ARAP
+    R_t = arap_exact(vert_diff_cdc, vert_diff_query, neigh, n_vert)
+    
     R_neigh_t = 0.5 * (
         torch.index_select(R_t, 0, neigh[:, 0])
         + torch.index_select(R_t, 0, neigh[:, 1])
     )
 
-    vert_diff_0_rot = torch.bmm(R_neigh_t, vert_diff_0.unsqueeze(2)).squeeze()
-    acc_t_neigh = vert_diff_t - vert_diff_0_rot
+    # Assuming R_neigh_t is correct, obtain the deformed coordinates such that they are ARAP deformed
+    vert_diff_query_rot = torch.bmm(R_neigh_t, vert_diff_query.unsqueeze(2)).squeeze() # obtain the new coordinates after deforming the shaper as rigid as possible 
+    acc_t_neigh = vert_diff_cdc - vert_diff_query_rot # Minimize the difference between deformed coords and original coords
 
-    E_arap = acc_t_neigh.norm() ** 2 + lambda_reg_len * (vert_t - vert_0).norm() ** 2
+    E_arap = acc_t_neigh.norm() ** 2 + lambda_reg_len * (vert_cdc - vert_query).norm() ** 2
 
     return E_arap
 

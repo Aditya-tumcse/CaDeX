4fb41fbdac1d7cf8c32e9605dc2c886925b9e7d9
Aditya-tumcse <srinivas.adi95@gmail.com>
2022-11-03 12:14:13 +0100

    feat : Introduced randomness in the sequence

--- dataset/oflow_dataset/subseq_dataset.py
+++ dataset/oflow_dataset/subseq_dataset.py
@@ -4,6 +4,8 @@ from torch.utils import data
 import yaml
 import logging
 import time
+import numpy as np
+import random
 
 
 class HumansDataset(data.Dataset):
@@ -101,9 +103,10 @@ class HumansDataset(data.Dataset):
         c_idx = self.metadata[category]["idx"]
 
         model_path = os.path.join(self.dataset_folder, category, model)
-        # number_of_files = os.listdir(os.path.join(model_path, "mesh_registered"))
-        # file_indices = list(np.arange(len(number_of_files)))
-        # required_file_indices = random.sample(file_indices,k=17)
+        # files = os.listdir(os.path.join(model_path,"mesh_registered"))
+        # folder_length = np.arange(len(files))
+        # file_indexes = random.sample(list(folder_length),k=17)
+
         _prepare_t = time.time() - _start_t
         data = {}
 
@@ -111,7 +114,7 @@ class HumansDataset(data.Dataset):
         for field_name, field in self.fields.items():
             _f_start_t = time.time()
             try:
-                field_data = field.load(model_path,idx, c_idx, start_idx)
+                field_data = field.load(model_path, idx, c_idx, start_idx)
                 debug_info += "[{} l{:.2f}".format(field_name, time.time() - _f_start_t)
             except Exception:
                 if self.no_except:

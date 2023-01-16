16fecdd0c9a26b0eba92e3c7d0aefe56e1ee2551
Aditya-tumcse <srinivas.adi95@gmail.com>
2023-01-16 09:26:28 +0100

    feat : integrated both mds and arap

--- core/models/cadex_dfaust.py
+++ core/models/cadex_dfaust.py
@@ -38,7 +38,7 @@ class Model(ModelBase):
             eval_metric += ["iou_t%d" % t]
             viz_mesh += ["mesh_t%d" % t]
         self.output_specs = {""
-            "metric": ["batch_loss", "loss_recon", "loss_corr","loss_arap"
+            "metric": ["batch_loss", "loss_recon", "loss_corr","loss_deform"
             ,"iou", "rec_error"] ## add loss_deform after loss_corr to log arap loss
             + eval_metric
             + ["loss_reg_shift_len"],
@@ -254,17 +254,17 @@ class ARAPBase(torch.nn.Module):
     
     def _loss_deform_single(self, query_vertices, canonical_vertices, neighbours):
         E_deform_list = []
-        #E_mds_list = []
+        E_mds_list = []
         torch.cuda.empty_cache()
         for i in range(query_vertices.shape[0]):
            
-            # E_y = arap_energy_exact(
-            #     query_vertices[i], canonical_vertices[i],neighbours
-            # )
+            E_y = arap_energy_exact(
+                query_vertices[i], canonical_vertices[i],neighbours
+            )
 
             
-            # if torch.isnan(E_y) ==  False:
-            #     E_deform_list.append(E_y)
+            if torch.isnan(E_y) ==  False:
+                E_deform_list.append(E_y)
             
            
             query_D = self.dist_mat(query_vertices[i],query_vertices[i],inplace=True)
@@ -278,10 +278,10 @@ class ARAPBase(torch.nn.Module):
         E_deform_tensor = torch.tensor(E_deform_list)
         E_deform_mean = torch.mean(E_deform_tensor)
 
-        # E_mds_tensor = torch.tensor(E_mds_list)
-        # E_mds_mean = torch.mean(E_mds_tensor)
+        E_mds_tensor = torch.tensor(E_mds_list)
+        E_mds_mean = torch.mean(E_mds_tensor)
         
-        return E_deform_mean.cuda()#, E_mds_mean.cuda()
+        return E_deform_mean.cuda(), E_mds_mean.cuda()
        
       
 
@@ -500,7 +500,7 @@ class CaDeX_DFAU(torch.nn.Module):
 
         output["batch_loss"] += deform_loss_mean
         
-        output["loss_arap"] = deform_loss_mean.detach()
+        output["loss_deform"] = deform_loss_mean.detach()
         
         if self.regularize_shift_len > 0.0:  # shift len loss
             regularize_shift_len_loss = shift.mean()
